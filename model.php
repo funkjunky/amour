00311faba0985bf70b3e46acb428931b2d601bf7
Jason McCarrell <jason@azzimov.com>
2013-04-19 17:44:05 -0400

    Added the model code, but haven't tested it in any way. Also replace to amourDB.

--- model.php
+++ model.php
@@ -3,31 +3,102 @@
  *	Notes:
  * There are two ways to identify the table, either name the class Model_<table>
  * or set the table_name variable in the extended class.
+ * the primary key is id by default, but it is encouraged to name your own key
+ * set it using id_key.
  */
 class Model implements serializable
 {
 	public $dbh;
-	public $cache;
-
-	private $table_name;
+	public $values;
 
 	private $id;
 
-	public function __constructor($id)
+	protected $table_name;
+	protected $id_key;
+
+	public function __constructor($id=null)
 	{
 		if(!isset($this->table_name))
 			$this->table_name = strtolower(substr(get_class($this), 6));
 
-		$this->id = $id;
+		$this->values = array();
+
+		if($id !== null)
+			$this->id = $this->values[$this->id_key] = $id;
+		//set the default id key to be 'id'
+		if(!isset($this->id_key))
+			$this->id_key = 'id';
 
 		$this->dbh = AmourDB::instance();
 	}
 
 	public function __get($key)
 	{
-		if(isset($this->cache[$key]))
-			return $this->cache[$key];
+		if(isset($this->values[$key]))
+			return $this->values[$key];
 
-		$this->dbh->select($this->table_name);
+		if(!isset($this->id))
+			die('JOO NEED AN ID TO GET A VALUE!'); //obviously need better error ha
+
+		$result = $this->dbh->select($this->table_name, array($key), array(
+			array($this->id_key, '=', $this->id),
+		));
+
+		return $this->values[$key] = $result[0][$key];
+	}
+
+	public function __set($key, $value)
+	{
+		$this->values[$key];
+	}
+
+	public function set($values)
+	{
+		$this->values = array_merge($values, $this->values);
+	}
+
+	//we just use replace, because it's a nice function...
+	public function save()
+	{
+		$this->dbh->replace($this->table_name, $this->values, array(
+			array($this->id_key, '=', $this->id),
+		));
+	}
+
+	//remove the item from the db and clears the values.
+	public function delete()
+	{
+		if(isset($this->id))
+			$this->dbh->delete($this->table_name, array(
+				array($this->id_key, '=', $this->id),
+			));
+
+		$this->values = array();
+	}
+
+	public static function instance($id = null)
+	{
+		return new self($id);
+	}
+
+	//we always get the id, because the model needs it.
+	public static function Find_all($columns = array('*'), $filters)
+	{
+		//add the id to our select list, so every model has a ref.
+		if($columns != array('*'))
+			$columns = array_merge($columns, array($this->id_key));
+
+		$results = $this->dbh->select(
+			$this->table_name,
+			implode_kvp($columns, ", ", " = "),
+			$filters
+		);
+
+		//iterate through the rows and create model objects to return
+		$models = array();
+		foreach($results as $row)
+			$models[$row[$this->id_key]] = self::instance()->set($row);
+
+		return $models;
 	}
 }
--- model/article.php
+++ model/article.php
@@ -1,4 +1,5 @@
 <?php
 class Model_Article extends Model
 {
+	protected $id_key = "article_id";
 }
